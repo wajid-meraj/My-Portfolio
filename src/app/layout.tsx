42b58daa6a3052113de7069daae2b90242225131
wajid-meraj <wajid4me7@gmail.com>
2026-01-11 15:10:49 +0530

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { ReactNode } from "react";
 
 export const metadata = {
   title: "Wajid Ansari | Portfolio",
   description: "Full Stack Web Developer Portfolio",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className="bg-gray-950 text-white">
