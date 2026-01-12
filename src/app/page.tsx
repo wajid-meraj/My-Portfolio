acac6594e910faa554696715348709e6a7ff5f9f
wajid-meraj <wajid4me7@gmail.com>
2026-01-12 17:40:40 +0530

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import Hero from "@/components/Hero";
+import About from "@/app/about/page";
+import Projects from "@/app/Projects/page";
+import Skills from "@/app/skills/page";
+import Contact from "@/app/Contact/page";
 
 export default function Home() {
   return (
     <main className="px-6">
       <Hero />
+              <About />
+              <Skills />
+              <Projects />
+              <Contact />
     </main>
   );
 }
